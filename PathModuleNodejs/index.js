d72878c215152d31cfef7f50810bd11ceb308c17
Mohitpahapalkar <122884642+Mohitpahapalkar@users.noreply.github.com>
2024-03-17 17:45:54 +0530

    Add files via upload

---
+++ PathModuleNodejs/index.js
@@ -0,0 +1,9 @@
+const path=require("path");
+
+// console.log(path.sep);
+
+// console.log(process.env.path);
+// console.log(path.delimiter);
+const currentFilePath=__filename;
+console.log(currentFilePath);
+
